403d7877f25731dd7f3c034f818c9cc381ece6ff
Sara <sarailic431@gmail.com>
2025-06-17 23:08:59 +0200

    setting innerHTML in components

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-function Project(props) {}
+function Project(props) {
+  const { inputProject } = props;
+  return (
+    <li className="project-item">
+      ${inputProject} <button class="delete-project-btn">X</button>
+    </li>
+  );
+}
 
 export default Project;
--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
-function ToDo(props) {}
+function ToDo(props) {
+  const { name, date } = props;
+  return (
+    <li className="to-do-item">
+      <p class="title-text">
+        {" "}
+        Title: <span class="title"> ${name} </span>{" "}
+      </p>
+      <div class="to-do-info">
+        <p class="date-text">
+          {" "}
+          Date: <span class="date"> ${date} </span>
+        </p>
+        <div class="btn-cont">
+          {" "}
+          <button class="edit-to-do-btn">edit</button>{" "}
+          <button class="delete-to-do-btn">delete</button>{" "}
+        </div>
+        <input class="check" type="checkbox" />
+      </div>
+    </li>
+  );
+}
 
 export default ToDo;
